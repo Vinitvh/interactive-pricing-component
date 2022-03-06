feefdb9c582511b7ba7d68b1772612249d954ded
Vinit Hemadri <vineet.vh@gmail.com>
2022-03-06 23:18:37 +0530

    Added info section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,12 @@
 import "../index.css";
+import checkIcon from "./assets/icon-check.svg";
 
 function Card() {
   return (
     <section>
       <div className="container">
         <div className="card">
-          <p className="heading__views">100k PAGEVIEWS</p>
+          <p className="heading__views">100K PAGEVIEWS</p>
           <p className="pricing">$16.00/month</p>
           <div className="toggle">
             <span className="billing__monthly">Monthly Billing</span>
@@ -16,7 +17,20 @@ function Card() {
             <span className="billing__yearly">Yearly Billing</span>
             <span className="billing__discount">-25%</span>
           </div>
-          <hr className="line"/>
+          <hr className="line" />
+          <div className="info">
+            <p>
+              <img src={checkIcon} alt="Check" /> Unlimited websites
+            </p>
+            <p>
+              <img src={checkIcon} alt="Check" /> 100% data ownership
+            </p>
+            <p>
+              <img src={checkIcon} alt="Check" />
+              Email reports
+            </p>
+            <button>Start my trial</button>
+          </div>
         </div>
       </div>
     </section>
